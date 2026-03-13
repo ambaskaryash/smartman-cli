7ed7c6948828bb7e9299174fcf94cb5073fa8eab
ambaskaryash <kimetsu119@gmail.com>
2026-03-14 00:25:19 +0530

    Update Homebrew formula with standard logic and hash

--- smartman.rb
+++ smartman.rb
@@ -1,15 +1,16 @@
 class Smartman < Formula
+  include Language::Python::Virtualenv
+
   desc "Modern Linux man page enhancer with AI explanations and TUI"
   homepage "https://github.com/ambaskaryash/smartman-cli"
   url "https://github.com/ambaskaryash/smartman-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_RELEASE"
+  sha256 "8af6f43faf0c47f000f57343baebb9125bd011c962474433ce782edeee551da4"
   license "MIT"
 
   depends_on "python@3.12"
-  depends_on "pipx"
 
   def install
-    system "pipx", "install", "--force", "."
+    virtualenv_install_with_resources
   end
 
   test do
